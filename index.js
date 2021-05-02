72d391e3ce41570f75f975fcdf2a603ac00bfa18
노드란 <78298771+rska59166@users.noreply.github.com>
2021-05-02 19:50:03 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+if (data.text == "!text" && adminList.includes(String(userId))) {
+        let bend = chat.rawAttachment.src_userId;
+        client.OpenLinkManager.kickUser(chat.channel, bend);
+        channel.sendChat(new node_kakao_1.ChatBuilder().append(new node_kakao_1.MentionContent(userInfo)).text("님, 킥완료").build(1));
+	}
+
+//아직 테스트 안해서 될지 안될지 모릅니다.//
