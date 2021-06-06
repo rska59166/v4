89a16b8ff590e44d05da2934f68990de21a2c3f3
노드란 <78298771+rska59166@users.noreply.github.com>
2021-06-06 23:13:37 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@
         channel.hideChat(new node_kakao.ChatBuilder().attachment({ logId: data.attachment().src_logId }).build({ logId: data.attachment().src_logId, type: data.attachment().src_type }));
         channel.sendChat(`완료`)
     }
-//가리기 명령어 사용시 가리고 싶은 문장,단어를 가리고 가리기 명령어도 가랍니다.
+//가리기 명령어 사용시 가리고 싶은 문장,단어를 가리고 가리기 명령어도 가립니다.
