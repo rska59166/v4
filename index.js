9cc80f4f0dd77dc3f94bca35445236d219f7c908
노드란 <78298771+rska59166@users.noreply.github.com>
2022-08-03 21:46:51 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,22 @@
         channel.hideChat(new node_kakao.ChatBuilder().attachment({ logId: data.attachment().src_logId }).build({ logId: data.attachment().src_logId, type: data.attachment().src_type }));
         channel.sendChat(`완료`)
     }
+if(data.text.startsWith("/업다운"){
+   channel.sendChat("업다운 게임을 시작합니다.");
+   chose =Math.floor(Math.random()*100);
+   }
+   if(data.text.startsWith("//")){
+      var num_2=Text.substr(2);
+      if(num_2 < chose){
+         channel.sendChat("Up");
+        } else if (num_2 > chose) {
+          channel.sendChat("Down");
+        } else if (num_2 == chose) {
+          channel.sendChat("정답!");
+        } else {
+          channel.sendChat(" 0~100 사이의 숫자를 입력해주세요!");
+        }
+      }
+
 //가리기 명령어 사용시 가리고 싶은 문장,단어를 가리고 가리기 명령어도 가립니다.
 
-//프로필 변경감지, 입퇴장 멘션, 전멘 등은 프라이빗으로 올릴 예정
